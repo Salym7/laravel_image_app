f9e411ea59eaefbb323e95c1d7dcda6a5932c045
Oleksandr Solianyk <salym3k@gmail.com>
2023-07-05 14:31:26 +0300

    Add vue

---
+++ resources/js/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center p-5">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">App</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {},
+}
+</script>
